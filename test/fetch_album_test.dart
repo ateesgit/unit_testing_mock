5879fa2ac1e31ba257c9ed9167062bc58baaca48
ateesgit <ateesgit@gmail.com>
2023-08-18 10:22:59 +0530

    completed code

--- test/fetch_album_test.dart
+++ test/fetch_album_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-// import 'package:mocking/main.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:unit_testing_mock/main.dart';
